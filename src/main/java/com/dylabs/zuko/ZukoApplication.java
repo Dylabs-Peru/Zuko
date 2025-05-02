e0e52bfcdc6fb06b7e7db90d7e40c235fb433e10
Turppie <codar2004@hotmail.com>
2025-05-02 17:53:50 -0500

    Primer commit del repositorio

---
+++ src/main/java/com/dylabs/zuko/ZukoApplication.java
@@ -0,0 +1,13 @@
+package com.dylabs.zuko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZukoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZukoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dylabs/zuko/ZukoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylabs.zuko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZukoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
