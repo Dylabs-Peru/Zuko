6d005c57a69e79ad3d4b4d5db8c7139e996c859a
MimiX07 <mildredmarchan.mq@gmail.com>
2025-05-06 16:53:32 -0500

    Añadir SongResponse DTO para la respuesta de canción

---
+++ src/main/java/com/dylabs/zuko/dto/response/SongResponse.java
@@ -0,0 +1,11 @@
+package com.dylabs.zuko.dto.response;
+
+import java.time.LocalDate;
+
+public record SongResponse(
+        Long id,
+        String title,
+        boolean isPublicSong,
+        LocalDate releaseDate,
+        String message
+) {}
