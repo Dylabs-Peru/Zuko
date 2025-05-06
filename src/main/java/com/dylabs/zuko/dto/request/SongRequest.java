0d40910dff07d9024ba15e0eab6aa944c57f906e
MimiX07 <mildredmarchan.mq@gmail.com>
2025-05-06 16:53:00 -0500

    Añadir SongRequest DTO para la solicitud de canción

---
+++ src/main/java/com/dylabs/zuko/dto/request/SongRequest.java
@@ -0,0 +1,12 @@
+package com.dylabs.zuko.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SongRequest(
+        @NotBlank(message = "El título es obligatorio")
+        @Size(min = 3, message = "El título debe tener más de 3 caracteres")
+        String title,
+
+        boolean isPublicSong
+) {}
