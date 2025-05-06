a275a41c4c9116385d05ef8b0d852ff59a3f517a
Turppie <codar2004@hotmail.com>
2025-05-05 23:10:31 -0500

    refactor: ajustar anotación RequestMapping en GenreController para corregir rutas en tests [US-13,14,15]

--- src/main/java/com/dylabs/zuko/controller/GenreController.java
+++ src/main/java/com/dylabs/zuko/controller/GenreController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/genres")
+@RequestMapping("genres")
 @RequiredArgsConstructor
 public class GenreController {
     private final GenreService genreService;
