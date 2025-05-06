c7fa7c177b0db201c398b8a3724a34f7d9357c6e
PANCONLOCRO <rosamariarv10@gmail.com>
2025-05-06 16:51:39 -0500

    feat: implementar dto

---
+++ src/main/java/com/dylabs/zuko/dto/request/CreateUserRequest.java
@@ -0,0 +1,27 @@
+package com.dylabs.zuko.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserRequest(
+        @NotBlank(message = "El nombre de usuario no puede estar vacío")
+        @Size(min = 3, message = "El nombre de usuario debe tener al menos tres caracteres")
+        String username,
+
+        @NotBlank(message = "El correo electrónico no puede estar vacío")
+        @Email(message = "El correo electrónico debe ser válido")
+        String email,
+
+        @NotBlank(message = "La contraseña no puede estar vacía")
+        @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+        String password,
+
+        String url_image, // Esto es opcional
+
+        @Size(max = 250, message = "La descripción no puede exceder 250 caracteres")
+        String description,
+
+        @NotBlank(message = "El rol no puede estar vacío")
+        String roleName // El nombre del rol, como "admin", "artista", etc.
+) {}
---
+++ src/main/java/com/dylabs/zuko/dto/response/UserResponse.java
@@ -0,0 +1,10 @@
+package com.dylabs.zuko.dto.response;
+
+public record UserResponse(
+        Long id,
+        String username,
+        String email,
+        String description,
+        String url_image,
+        String roleName // El nombre del rol asignado
+) {}
