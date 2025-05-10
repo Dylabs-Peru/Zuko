43584a22c474a277ee0b61e72f4a6c438ff5641d
YuhiTTo <luceromanchayparedes@gmail.com>
2025-05-10 00:28:23 -0500

    Feat:Añadir cambios del create-artist

--- src/main/java/com/dylabs/zuko/controller/ArtistController.java
+++ src/main/java/com/dylabs/zuko/controller/ArtistController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("artists")
+@RequestMapping("/api/v1/artists")
 @RequiredArgsConstructor
 
 public class ArtistController {
