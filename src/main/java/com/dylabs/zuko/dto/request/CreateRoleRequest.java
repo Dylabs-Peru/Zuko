00fb1ab94f691c01c07c4bbb612c7e402f1a3828
Rosa Maria Rodriguez Valencia <132715227+panconlocro@users.noreply.github.com>
2025-05-13 21:52:12 -0500

    Update CreateRoleRequest.java

--- src/main/java/com/dylabs/zuko/dto/request/CreateRoleRequest.java
+++ src/main/java/com/dylabs/zuko/dto/request/CreateRoleRequest.java
@@ -8,7 +8,7 @@ public record  CreateRoleRequest (
         @NotBlank(message="El nombre no puede estar vacío")
         @Size(min=3, message = "El nombre del rol debe tener al menos tres caracteres")
         String roleName,
-        @Size(max=250,message = "La descripción no puede exceder 200 caracteres")
+        @Size(max=250,message = "La descripción no puede exceder 250 caracteres")
         String description
 ){
 }
