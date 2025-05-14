4caa6258ddbf1656a73d9ab0a4e5d09a84ec6395
Rosa Maria Rodriguez Valencia <132715227+panconlocro@users.noreply.github.com>
2025-05-13 23:16:45 -0500

    Revert "feat(playlist): add all"

--- src/main/java/com/dylabs/zuko/exception/playlistExceptions/PlaylistAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.dylabs.zuko.exception.playlistExceptions;
-
-public class PlaylistAlreadyExistsException extends RuntimeException {
-    public PlaylistAlreadyExistsException(String message) {
-        super(message);
-    }
-}
