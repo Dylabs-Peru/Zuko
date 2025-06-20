075d04061d39c4a0c7e80e264098d68038a71c3c
MimiX07 <mildredmarchan.mq@gmail.com>
2025-06-19 19:14:32 -0500

    feat(repository): agregar métodos para buscar canciones por artista y título público

--- src/main/java/com/dylabs/zuko/repository/SongRepository.java
+++ src/main/java/com/dylabs/zuko/repository/SongRepository.java
@@ -3,8 +3,11 @@ package com.dylabs.zuko.repository;
 import com.dylabs.zuko.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SongRepository extends JpaRepository<Song, Long> {
     boolean existsByTitleIgnoreCase(String title);
     boolean existsByTitleIgnoreCaseAndArtistId(String title, Long artistId);
-
+    List<Song> findByTitleContainingIgnoreCaseAndIsPublicSongTrue(String title);
+    List<Song> findAllByArtistId(Long artistId);
 }
