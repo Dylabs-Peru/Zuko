597f2a9f72724ebeccc48f211999db5911a1c357
SeuNg720 <josemayhua.12@hotmail.com>
2025-06-24 00:24:53 -0500

    Feat: Arreglar manejo al crear un Album

--- src/main/java/com/dylabs/zuko/dto/request/AlbumRequest.java
+++ src/main/java/com/dylabs/zuko/dto/request/AlbumRequest.java
@@ -3,7 +3,6 @@ package com.dylabs.zuko.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import java.util.List;
 
 public record AlbumRequest(
@@ -15,7 +14,6 @@ public record AlbumRequest(
 
         String cover,
 
-        @NotNull(message = "El ID del artista es obligatorio")
         Long artistId,
 
         @NotNull(message = "El ID del género es obligatorio")
