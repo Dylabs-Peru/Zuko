3585738fcf52555acd6d4b4bd7a7de817ab41fdf
MimiX07 <mildredmarchan.mq@gmail.com>
2025-07-01 12:41:22 -0500

    Cambio del repository

--- src/main/java/com/dylabs/zuko/repository/SongRepository.java
+++ src/main/java/com/dylabs/zuko/repository/SongRepository.java
@@ -3,7 +3,10 @@ package com.dylabs.zuko.repository;
 import com.dylabs.zuko.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
     boolean existsByTitleIgnoreCase(String title);
@@ -11,4 +14,6 @@ public interface SongRepository extends JpaRepository<Song, Long> {
     List<Song> findByTitleContainingIgnoreCaseAndIsPublicSongTrue(String title);
     List<Song> findAllByArtistId(Long artistId);
     List<Song> findAll();
+    List<Song> findTop3ByIsPublicSongTrueAndReleaseDateOrderByIdDesc(LocalDate releaseDate);
+
 }
